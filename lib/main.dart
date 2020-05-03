1c9d9cd02161526ab246fb54424e22d41db04657
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-03 11:32:15 +0700

    7 buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,7 +9,66 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                color: Colors.red,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note1.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note2.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note3.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note4.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note5.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note6.wav');
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                child: null,
+                onPressed: () {
+                  final audio = AudioCache();
+                  audio.play('note7.wav');
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
